dba60c643e076a96b1754f59851b8477d6dd6281
Joao Rouxinol <>
2022-02-24 18:59:15 +0000

    Last Commit before merge to main

--- PokeDexIOS/PokeDexIOS/Model/Pokemon.swift
+++ PokeDexIOS/PokeDexIOS/Model/Pokemon.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+
+struct pokemon: Codable, Equatable{
+    static func == (lhs: pokemon, rhs: pokemon) -> Bool {
+        if lhs.name == rhs.name{
+            return true
+        }
+        return false
+    }
+    
+      let id: Int
+      let types: [atype]
+      let abilities: [possibleAbility]
+      let moves: [possibleMoves]
+      let height: Int
+      let weight: Int
+      let name: String
+      let sprites: sprite
+}
+
+struct atype: Codable{
+      let slot: Int
+      let type: type
+}
+//
+struct type: Codable{
+    let name: String
+}
+
+struct sprite: Codable{
+    let back_default: String
+    let front_default: String
+}
+
+
+struct possibleMoves: Codable{
+    let move: move
+}
+
+struct move: Codable{
+    let name: String
+}
+
+struct possibleAbility: Codable{
+    let ability: ability
+}
+
+struct ability: Codable{
+    let name: String
+}
