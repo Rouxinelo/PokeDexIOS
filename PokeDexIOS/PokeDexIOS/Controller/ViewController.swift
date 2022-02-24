ca3c160884d02c5361396c3ece0886285232670a
Joao Rouxinol <>
2022-02-24 22:05:48 +0000

    Added a function to load an image from a url

--- PokeDexIOS/PokeDexIOS/Controller/ViewController.swift
+++ PokeDexIOS/PokeDexIOS/Controller/ViewController.swift
@@ -138,3 +138,19 @@ extension ViewController: UITableViewDataSource{
         return cell
     }
 }
+
+// MARK - Load Image From URL
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
