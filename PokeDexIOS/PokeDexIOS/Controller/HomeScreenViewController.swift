fa6eda1967854b904690345f5320597984dbf6b1
Joao Rouxinol <>
2022-02-25 14:09:30 +0000

    Added segue (initial screen -> pokedex screen)

--- PokeDexIOS/PokeDexIOS/Controller/HomeScreenViewController.swift
+++ PokeDexIOS/PokeDexIOS/Controller/HomeScreenViewController.swift
@@ -9,21 +9,16 @@ import UIKit
 
 class HomeScreenViewController: UIViewController {
 
+    
+    @IBAction func goToPokedex(_ sender: UIButton) {
+        performSegue(withIdentifier: "toPokeDex", sender: sender)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
