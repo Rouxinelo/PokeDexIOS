b470a925b4076ad5de61562a2b8ac20695b26ce0
Joao Rouxinol <>
2022-03-11 12:43:53 +0000

    Added MoveStats View and MoveStatsViewController Added segue from MovesAndAbilities to MoveStats

---
+++ PokeDexIOS/PokeDexIOS/Controller/MoveStatsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MoveStatsViewController.swift
+//  PokeDexIOS
+//
+//  Created by Joao Rouxinol on 11/03/2022.
+//
+
+import UIKit
+
+class MoveStatsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
