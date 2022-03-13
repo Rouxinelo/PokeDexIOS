077a195e24067e29c8b5a73786d042baeb2b4db5
Joao Rouxinol <>
2022-03-13 17:33:52 +0000

    Created ApiPath file

---
+++ PokeDexIOS/PokeDexIOS/ApiPath.swift
@@ -0,0 +1,57 @@
+//
+//  ApiPath.swift
+//  PokeDexIOS
+//
+//  Created by Joao Rouxinol on 13/03/2022.
+//
+
+import Foundation
+
+let URLPath = "https://pokeapi.co/api/v2/"
+let WebhookPath = "https://webhook.site/1f06d0d2-e48f-4135-90e3-e78a8e5b894f"
+
+protocol TargetType {
+    var baseURL: String { get }
+    var path: String { get }
+    var method: String { get }
+}
+
+enum API {
+    case GetPokedex(String, String)
+    case GetSpecies(String)
+    case GetPokemonInfo(String)
+}
+
+extension API: TargetType {
+
+    public var baseURL: String {
+        return URLPath
+    }
+
+    public var path: String {
+        switch self {
+        case .GetPokedex(let offset, let pageSize):
+            let query = "pokemon?"
+            let offset = "offset=" + offset
+            let limit = "&limit=" + pageSize
+            return "\(baseURL)" + query + offset + limit
+        case .GetSpecies(let dexId):
+            let query = "pokemon-species/"
+            return "\(baseURL)" + query + dexId
+        case .GetPokemonInfo(let dexId):
+            let query = "pokemon/"
+            return "\(baseURL)" + query + dexId
+        }
+    }
+
+    public var method: String {
+        switch self {
+        case .GetPokedex:
+            return "GET"
+        case .GetSpecies:
+            return "GET"
+        case .GetPokemonInfo:
+            return "GET"
+        }
+    }
+}
