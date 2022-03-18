4208066b11f3145875a13c10c6901be5d31c17f9
Joao Rouxinol <>
2022-03-18 17:23:23 +0000

    Created view controller for the main menu view Connected View to controller

---
+++ PokeDexIOS/PokeDexIOS/Controller/MainMenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainMenuViewController.swift
+//  PokeDexIOS
+//
+//  Created by Joao Rouxinol on 18/03/2022.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("BUERERE")
+    }
+    
+
+}
