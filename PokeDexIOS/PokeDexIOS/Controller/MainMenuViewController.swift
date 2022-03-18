ca8c8f9eec6acfafa93f47aa1f0203d7c85986b4
Joao Rouxinol <>
2022-03-18 18:19:50 +0000

    Added Switch to MainMenu and a label to display mode Switch and Label now detect which Appearance the phone is running (Light or Dark)

--- PokeDexIOS/PokeDexIOS/Controller/MainMenuViewController.swift
+++ PokeDexIOS/PokeDexIOS/Controller/MainMenuViewController.swift
@@ -9,8 +9,21 @@ import UIKit
 
 class MainMenuViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var modeLabel: UILabel!
+    @IBOutlet weak var modeSwitch: UISwitch!
+    
     // MARK: - IBActions
     
+    @IBAction func switchPressed(_ sender: UISwitch) {
+        switch sender.isOn {
+        case true:
+            modeLabel.text = "Dark"
+        default:
+            modeLabel.text = "Light"
+        }
+    }
     @IBAction func pokedexButtonClicked(_ sender: UIButton) {
         performSegue(withIdentifier: K.Segues.mainMenuToPokedex, sender: self)
     }
@@ -19,21 +32,27 @@ class MainMenuViewController: UIViewController {
         performSegue(withIdentifier: K.Segues.mainMenuToAboutMe, sender: self)
     }
     
+    // MARK: - Other functions
+    
+    func setSwitch(){
+        switch UIScreen.main.traitCollection.userInterfaceStyle.rawValue {
+        case 1:
+            modeLabel.text = "Light"
+            modeSwitch.isOn = false
+        case 2:
+            modeLabel.text = "Dark"
+            modeSwitch.isOn = true
+        default:
+            modeLabel.text = "Light"
+            modeSwitch.isOn = false
+        }
+    }
+    
     // MARK: - View Did Load
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setSwitch()
     }
 
-}
-
-// MARK: - Prepare for segue
-
-extension MainMenuViewController {
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == K.Segues.mainMenuToPokedex {
-            let VC = segue.destination as! PokedexViewController
-        }
-    }
 }
